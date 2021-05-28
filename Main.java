0e3c0e8b0a197f693744e5c18d678db945c6c954
Syed Gibran Ahmed <43906549+syedgibran96@users.noreply.github.com>
2021-05-28 18:48:43 +0530

    added print statement

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ class Main
     public static void main(String args[])
     {
         //fill the code;
-      System.out.println("Hello world this is jenkins);
+      System.out.println("Hello world this is jenkins");
       System.out.println("Running the build stage in jankins");
     }
 }
